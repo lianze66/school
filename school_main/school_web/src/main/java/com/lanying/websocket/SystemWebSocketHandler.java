8d2c058c4c2df2e7d9cf42837a8ffe6ff56442a9
lianze <312239109@qq.com>
2018-09-14 14:54:44 +0800

    111

---
+++ school_main/school_web/src/main/java/com/lanying/websocket/SystemWebSocketHandler.java
@@ -0,0 +1,43 @@
+package com.lanying.websocket;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+public class SystemWebSocketHandler implements WebSocketHandler {
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session)
+			throws Exception {
+		System.out.println("websocket连接成功。");
+	}
+
+	@Override
+	public void handleMessage(WebSocketSession session,
+			WebSocketMessage<?> message) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleTransportError(WebSocketSession session,
+			Throwable exception) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session,
+			CloseStatus closeStatus) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean supportsPartialMessages() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- school_main/school_web/src/main/webapp/index.jsp
+++ school_main/school_web/src/main/webapp/index.jsp
@@ -5,23 +5,25 @@
 	<head>
 		<title>首页</title>
 		<script type="text/javascript">
-			var url = "ws://127.0.0.1:8080/school_web/websocket/sendMessage";
-			var ws;
-			if("WebSocket" in window) {
-				ws = new WebSocket(url);
-			} else if("MozWebSocket" in window) {
-				ws = new MozWebSocket(url);
-			} else {
-				alert("您的浏览器版本过低，不支持websocket。");
+			function sendMessage() {
+				var url = "ws://127.0.0.1:8080/school_web/websocket/sendMessage";
+				var ws;
+				if("WebSocket" in window) {
+					ws = new WebSocket(url);
+				} else if("MozWebSocket" in window) {
+					ws = new MozWebSocket(url);
+				} else {
+					alert("您的浏览器版本过低，不支持websocket。");
+				}
+				
+				ws.onopen = function() {
+					alert("AAA");
+				};
 			}
-			
-			ws.onopen = function() {
-				alert("AAA");
-			};
 		</script>
 	</head>
 	
 	<body>
-		<h2>Hello World!</h2>
+		<input type="button" value="测试websocket" onclick="sendMessage();" />
 	</body>
 </html>
