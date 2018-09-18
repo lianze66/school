b04e9be465023eee64bfd517d172f44cdb3cb91f
lianze <312239109@qq.com>
2018-09-18 17:01:22 +0800

    增加了redis.

--- school_main/school_web/src/main/java/com/lanying/controller/SysUserController.java
+++ school_main/school_web/src/main/java/com/lanying/controller/SysUserController.java
@@ -3,6 +3,8 @@ package com.lanying.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +19,21 @@ public class SysUserController {
 	@Autowired
 	private ISysUserService sysUserService;
 	
+	@Autowired
+	private RedisTemplate redisTemplate;
+	
 	@ResponseBody
 	@RequestMapping("queryList.do")
 	public List<SysUser> queryList() {
 		return sysUserService.queryList();
+	}
+	
+	@ResponseBody
+	@RequestMapping("querySchoolName.do")
+	public String querySchoolName() {
+		ValueOperations opsForValue = redisTemplate.opsForValue();
+		opsForValue.set("bbb", "222");
+		String name = opsForValue.get("school_name").toString();
+		return name;
 	}
 }
