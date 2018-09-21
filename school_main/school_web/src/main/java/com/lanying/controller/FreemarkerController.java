260561fa35e0e35927e6e4ca8886c2433ba7818b
lianze <312239109@qq.com>
2018-09-21 10:27:28 +0800

    11

--- school_main/school_web/src/main/java/com/lanying/controller/FreemarkerController.java
+++ school_main/school_web/src/main/java/com/lanying/controller/FreemarkerController.java
@@ -1,20 +1,76 @@
 package com.lanying.controller;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import com.lanying.dto.Student;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 
 @Controller
 public class FreemarkerController {
+	
+	@Autowired
+	private FreeMarkerConfigurer freeMarkerConfigurer;
 	
 	@ResponseBody
 	@RequestMapping("showFreemarker")
 	public ModelAndView showFreemarker() {
 		ModelAndView mav = new ModelAndView("may");
 		
+		Student stu0 = new Student(1, "小花", "女");
+		Student stu1 = new Student(2, "小张", "男");
+		Student stu2 = new Student(3, "小王", "男");
+		Student stu3 = new Student(4, "小丽", "女");
+		
+		List<Student> list = new ArrayList<Student>();
+		list.add(stu1);
+		list.add(stu2);
+		list.add(stu3);
+		
 		mav.addObject("userName", "小明");
+		mav.addObject("student", stu0);
+		mav.addObject("stuList", list);
+		mav.addObject("cur_time", new Date());
+		
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("name", "廉泽");
+		
+		createStatic("test.ftl", "D:/lianze.html", data);
 		
 		return mav;
+	}
+	
+	private void createStatic(String templateFileName, String createFilePath, Map<String, Object> data) {
+		Writer out = null;
+		try {
+			Configuration configuration = freeMarkerConfigurer.getConfiguration();
+			Template template = configuration.getTemplate(templateFileName);
+			out = new FileWriter(new File(createFilePath));
+			template.process(data, out);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- school_main/school_web/src/main/webapp/WEB-INF/ftl/may.ftl
+++ school_main/school_web/src/main/webapp/WEB-INF/ftl/may.ftl
@@ -1,1 +1,33 @@
-用户名称:${userName}
+<html>
+	<head>
+		<title>Freemarker练习</title>
+	</head>
+	<body>
+		用户名称:${userName}
+		<br />
+		学生名称：${student.name}  学生性别：${student.sex}
+		<br />
+		<#list stuList as stu>
+			学生名称：${stu.name}  学生性别：${stu.sex}<br />
+		</#list>
+		<br />
+		<#assign x>
+			<#list ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天"] as n> ${n}</#list>
+       </#assign>
+       ${x}
+       <br />
+       
+       
+       <#list ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天"] as n>
+       		<#if n_index == 1 || (n_index == 3)>
+       			<font color='red'>${n}</font>
+       		 <#else>
+       		 	<font color='blue'>${n}</font>
+       		</#if>
+       	</#list>
+       	<br />
+       	
+       	${cur_time?time}
+       	
+	</body>
+</html>
